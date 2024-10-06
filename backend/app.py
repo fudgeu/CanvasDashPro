46dd6d7895b119c27e58e674b0b1d5d1fbf6f621
John Liu <jliu@finefine.us>
2024-10-06 04:58:11 -0400

    perf: Further prompt modifying for better generation quality

--- backend/app.py
+++ backend/app.py
@@ -79,7 +79,9 @@ def assignment_analyze():
 
         inputs = [system1, user_input]
 
-        output = run("@cf/meta/llama-3.2-3b-instruct", inputs)
+        # output = run("@cf/meta/llama-3.2-3b-instruct", inputs)
+        output = run("@cf/meta/llama-3-8b-instruct", inputs)
+
 
         try:
             print(output)
@@ -120,8 +122,8 @@ def announcement_keywords():
                     "announcementTitle": "the announcement's title",
                     "announcementBody": "the announcement's body"
                 }
-                Generate a one-sentence summary in third-person perspective from the given data.
-                Make sure the response is in JSON format like this:
+                Generate a one-sentence summary less than 25 words long in third-person perspective from the given data.
+                Make sure the response is strictly following in JSON format like this and there should be no other content that will effect the JSON parsing process:
                 {"body": "summary..."}
             '''
         }
@@ -140,7 +142,8 @@ def announcement_keywords():
 
         inputs = [system1, user_input]
 
-        output = run("@cf/meta/llama-3.2-3b-instruct", inputs)
+        # output = run("@cf/meta/llama-3.2-3b-instruct", inputs)
+        output = run("@cf/meta/llama-3-8b-instruct", inputs)
 
         try:
             print(output)
